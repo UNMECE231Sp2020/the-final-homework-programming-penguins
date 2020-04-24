a82085c44814a04fb5f88fff3f31d5ff404c5193
Fviramontes8 <kuolemajarvi@yahoo.com>
2020-04-23 23:24:15 -0600

    Added hints prefixed with TODO

--- GeneralStack.hpp
+++ GeneralStack.hpp
@@ -15,6 +15,7 @@ class Stack {
 		
 		//Default copy constructor
 		Stack(const Stack &stack) {
+			//TODO: Why not access stack's _data attribute?
 			//_data = 
 			_size = stack.size();
 		}
@@ -55,6 +56,8 @@ class Stack {
 		//This overloaded operator is empty, please implement
 		Stack<T> operator=(const Stack<T> &stack) {
 			_size = stack.size();	
+			//TODO: why not copy _data from stack to the interal 
+			// _data like you did with _size?
 			_data();		//reconstruct with nothing in it
 			return this;
 		}
